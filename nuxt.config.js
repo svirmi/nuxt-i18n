cae6b089709bd76725f2419e07aed992eddcf81c
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-12-27 20:12:29 +0200

    initial setup

---
+++ nuxt.config.js
@@ -0,0 +1,70 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'Title smth',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    'nuxt-i18n',
+  ],
+
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        file: 'en.js'
+      },
+      {
+        code: 'es',
+        file: 'es.js'
+      },
+      {
+        code: 'ru',
+        file: 'ru.js'
+      }
+    ],
+    defaultLocale: 'en',
+    vueI18n: {
+      fallbackLocale: 'en',
+    },
+    lazy: true,
+    langDir: 'lang/',
+    defaultLocale: 'en'
+  },
+
+  // Content module configuration (https://go.nuxtjs.dev/config-content)
+  content: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
